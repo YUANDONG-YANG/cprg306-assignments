0d34f1442b17e952c484123235c23e6a5794f874
YUANDONG-YANG <yuandong.yang@edu.sait.ca>
2025-09-11 10:34:26 -0600

    Add week-2 assignment files

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import "./home.css"; // import custom styles
 
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        Go to <Link href="/week-2">Week 2 Assignment</Link>
+        {/* Add a clear instruction for the user */}
+        Click here to open:{" "}
+        <Link href="/week-2" className="week2-link">
+          Week 2 Assignment
+        </Link>
       </p>
       <p>Author: Yang Yuandong</p>
     </main>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
+import "./student-info.css"; // import custom styles
 
 export default function StudentInfo() {
   return (
     <div>
-      <p>Your Name: Yang Yuandong</p>
+      <p>Auth: Yang Yuandong</p>
       <p>
         GitHub:{" "}
-        <Link href="https://github.com/YUANDONG-YANG/cprg306-assignments">
+        <Link
+          href="https://github.com/YUANDONG-YANG/cprg306-assignments"
+          className="github-link"
+        >
           cprg306-assignments
         </Link>
       </p>
