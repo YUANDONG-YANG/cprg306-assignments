75dae70dad5aa0de592f84fa306df4864f82d81d
YUANDONG-YANG <yuandong.yang@edu.sait.ca>
2025-11-10 12:47:36 -0600

    week-9

--- app/week-9/_utils/firebase.js
+++ app/week-9/_utils/firebase.js
@@ -3,19 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
-console.log("🔍 env param：");
-console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-console.log("API Key length:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.length);
-console.log("API Key has quotes?", process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.includes('"'));
-const firebaseConfig = {
-  apiKey: "AIzaSyCW0uggHy2CP6610u0dVtcAxViZhyTBDiM",
-  authDomain: "cprg306-demos-d-v1-8411f.firebaseapp.com",
-  projectId: "cprg306-demos-d-v1-8411f",
-  storageBucket: "cprg306-demos-d-v1-8411f.firebasestorage.app",
-  messagingSenderId: "174102940597",
-  appId: "1:174102940597:web:8870b3c8c3ba621c9ba7fc"
-};
-
+console.log("🔥 Firebase Config:", firebaseConfig);
 // Your web app's Firebase configuration
 // const firebaseConfig = {
 //   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -25,8 +13,14 @@ const firebaseConfig = {
 //   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 //   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 // };
-
-console.log("🔥 Firebase Config:", firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyCW0uggHy2CP6610u0dVtcAxViZhyTBDiM",
+  authDomain: "cprg306-demos-d-v1-8411f.firebaseapp.com",
+  projectId: "cprg306-demos-d-v1-8411f",
+  storageBucket: "cprg306-demos-d-v1-8411f.firebasestorage.app",
+  messagingSenderId: "174102940597",
+  appId: "1:174102940597:web:8870b3c8c3ba621c9ba7fc"
+};
 // Initialize Firebase
 // const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 // export const auth = getAuth(app);
